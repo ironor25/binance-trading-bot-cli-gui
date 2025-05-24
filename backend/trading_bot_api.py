8cef9dc6656ec5085087f09ec55adc167897e362
Ironor25 <rock.on0103@gmail.com>
2025-05-24 22:45:55 +0530

    some_audit

--- backend/trading_bot_api.py
+++ backend/trading_bot_api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI , HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from trading_bot import BasicBot
+from trading_bot_cli import BasicBot
 from dotenv import load_dotenv
 import os
 load_dotenv()
